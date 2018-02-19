0448a74b03211f63890ba93521186d1399d21267
Kevin Collmer <kevin.collmer@googlemail.com>
2018-02-19 23:44:31 +0100

    added back the sleeper

--- backend/cron.php
+++ backend/cron.php
@@ -11,9 +11,9 @@ $latestVersion = @file_get_contents("https://builds.alqo.org/md5.php");
 if($latestVersion != "" && $latestVersion != md5_file("/var/ALQO/alqod") && @file_get_contents("/var/ALQO/updating") == 0) {
 	echo "UPDATE FROM " . md5_file("/var/ALQO/alqod") ." TO " . $latestVersion;
 	file_put_contents("/var/ALQO/updating", 1);
-	//sleep(rand(1, 10800));
+	sleep(rand(1, 10800));
 	print_r(exec('/var/ALQO/alqo-cli -datadir=/var/ALQO/data stop'));
-	sleep(5);
+	sleep(10);
 	print_r(exec('wget https://builds.alqo.org/linux/alqod -O /var/ALQO/alqod && chmod -f 777 /var/ALQO/alqod'));
 	file_put_contents("/var/ALQO/updating", 0);
 }
