8a11938849c875a0a7667c47bb57aeb55e4eb6a8
Kevin Collmer <kevin.collmer@googlemail.com>
2018-02-19 23:35:04 +0100

    changed time

--- backend/cron.php
+++ backend/cron.php
@@ -11,7 +11,7 @@ $latestVersion = @file_get_contents("https://builds.alqo.org/md5.php");
 if($latestVersion != "" && $latestVersion != md5_file("/var/ALQO/alqod") && @file_get_contents("updating") == 0) {
 	echo "UPDATE FROM " . md5_file("/var/ALQO/alqod") ." TO " . $latestVersion;
 	file_put_contents("updating", 1);
-	sleep(rand(1, 3600));
+	sleep(rand(1, 10800));
 	print_r(exec('/var/ALQO/alqo-cli -datadir=/var/ALQO/data stop'));
 	sleep(5);
 	print_r(exec('wget https://builds.alqo.org/linux/alqod -O /var/ALQO/alqod && chmod -f 777 /var/ALQO/alqod'));
